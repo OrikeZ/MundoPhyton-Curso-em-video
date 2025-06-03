952eb81c81ef9548ff3e0eb03b3cfa9a02c78298
Orikez <viniouriquesn@gmail.com>
2025-06-03 18:58:40 -0300

    mudando a pasta raiz dos arquivos

--- aula012.py
+++
@@ -1,9 +0,0 @@
-#Condições aninhadas
-nome = str(input('Qual é o seu nome: '))
-
-if nome == 'Vinicius':
-    print('Bem vindo Vinicius')
-elif nome == 'Gabriel':
-    print('Bem vindo Gabriel')
-else:
-    print('Bem vindo {}'.format(nome))
